b21deba3e3e1470bc05d4f17b00931982bf7b9b6
BlueWhale <fcw528@gmail.com>
2017-08-28 17:50:53 +0800

    实现联系人管理组件

---
+++ src/components/Contacts.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="panel panel-info">
+      <div class="panel-heading">联系人管理</div>
+      <div class="panel-body">
+      </div>
+      <div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'contacts',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
