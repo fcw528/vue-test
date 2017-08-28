f9750565c18c6c8c5a98004fcf8a89806b280aea
BlueWhale <fcw528@gmail.com>
2017-08-28 17:55:37 +0800

    实现联系人网格组件

---
+++ src/components/ContactsGrid.vue
@@ -0,0 +1,98 @@
+<template>
+  <Table border stripe :columns="columns" :data="data"></Table>
+</template>
+<script>
+  export default {
+    name: 'contacts_grid',
+    data () {
+      return {
+        columns: [
+          {
+            title: 'first_name',
+            key: 'first_name',
+            align: 'center'
+          },
+          {
+            title: 'last_name',
+            key: 'last_name',
+            align: 'center'
+          },
+          {
+            title: 'email',
+            key: 'email',
+            align: 'center'
+          },
+          {
+            title: 'description',
+            key: 'description',
+            align: 'center'
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 200,
+            align: 'center',
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.show(params.row)
+                    }
+                  }
+                }, '查看'),
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.update(params.row)
+                    }
+                  }
+                }, '修改'),
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.delete(params.row)
+                    }
+                  }
+                }, '删除')
+              ])
+            }
+          }
+        ],
+        data: this.getList()
+      }
+    },
+    methods: {
+      getList () {
+        // todo
+      },
+      show (row) {
+        // todo
+      },
+      update (row) {
+        // todo
+      },
+      delete (row) {
+        // todo
+      }
+    }
+  }
+</script>
