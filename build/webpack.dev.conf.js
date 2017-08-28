a0773699c68b1bbaabd152518c1bb957ff291506
BlueWhale <fcw528@gmail.com>
2017-08-28 17:27:28 +0800

    修改项目配置用于调试、安装项目要用到的js模块

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -16,7 +16,9 @@ module.exports = merge(baseWebpackConfig, {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // cheap-module-eval-source-map is faster for development
-  devtool: '#cheap-module-eval-source-map',
+  // devtool: '#cheap-module-eval-source-map',
+  // 方便调试javascript
+  devtool: '#source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
+Vue.use(iView)
 
 export default new Router({
   routes: [
